ff06f64c4967b2f5f29c66431847a1a51f6788eb
Jeff Mitchell <jeff@foschimitchell.ca>
2021-04-16 06:41:55 -0700

    Added logic to display a video

--- src/scripts.js
+++ src/scripts.js
@@ -3,25 +3,42 @@
 // is then displayed in a simple web page.
 
 window.onload = () => {
-    nasaPhotoApi()
+    nasaPhotoApi();
 }
 
+// fetch() data, returns in JSON format
 const nasaPhotoApi = () => {
     fetch('https://api.nasa.gov/planetary/apod?api_key=lsULnkmChaJlS3fZO85M3cnGA8TFCAm2peEfd9QS')
      .then((response) => {
          return response.json()
      }).then((data) => {
          console.log(data)
-         showNasaPhotoData(data)
-     })
+         if ( data.url.includes("youtube") ) {  // parse the url in the JSON data to see if it's a photo or video
+            showNasaVideo(data)
+         } else showNasaPhoto(data)
+     });
 }
 
-showNasaPhotoData = (photo) => {
-    document.getElementById('date').innerText = photo.date
-    document.getElementById('title').innerText = photo.title
-    document.getElementById('explanation').innerText = photo.explanation
-    let image = document.createElement('img')
-    image.setAttribute('src', photo.url)
-    image.setAttribute('alt', "NASA photo of the day")
-    document.body.appendChild(image).classList.add('img')
+// display the photo in a web page
+const showNasaPhoto = (photo) => {
+    document.getElementById('date').innerText = photo.date;
+    document.getElementById('title').innerText = photo.title;
+    document.getElementById('explanation').innerText = photo.explanation;
+    let imgFrame = document.createElement('img');
+    imgFrame.setAttribute('src', photo.url);
+    imgFrame.setAttribute('alt', "NASA photo of the day");
+    document.body.appendChild(imgFrame).classList.add('img');
+}
+
+// display the video in a web page
+const showNasaVideo = (video) => {
+    document.getElementById('date').innerText = video.date;
+    document.getElementById('title').innerText = video.title;
+    document.getElementById('explanation').innerText = video.explanation;
+    let vidFrame = document.createElement('iframe');
+    vidFrame.setAttribute('src', video.url);
+    vidFrame.setAttribute('alt', "NASA video of the day");
+    vidFrame.style.width = "1024px";
+    vidFrame.style.height = "768px";
+    document.body.appendChild(vidFrame).classList.add('video');
 }
