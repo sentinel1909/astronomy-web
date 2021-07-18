b26870398105a9bbc9cd9fccd83d762fa57fa4f6
Jeff Mitchell <sentinel1909@protonmail.com>
2021-07-18 10:12:33 -0700

    Refactoring to improve consistency of code

--- src/scripts.js
+++ src/scripts.js
@@ -13,34 +13,39 @@ const nasaPhotoApi = () => {
         .then(response => response.json())
         .then(apiData => {
             if ( apiData.url.includes("youtube") ) {    // check for the word "youtube" in the url that's returned from the api data
-                showNasaVideo(apiData)       // if the word "youtube" is there, it means there's video and needs to be rendered differently   
-            } else showNasaPhoto(apiData)   // if it's a photo, pass it to the photo rendering function
-        })
+                showText(apiData)
+                showVideo(apiData)       // if the word "youtube" is there, it means there's video and needs to be rendered differently   
+            } else {
+                showText(apiData)
+                showPhoto(apiData)   // if it's a photo, pass it to the photo rendering function
+        }})
         .catch((error) => {                             // catch an error and log it to console if the NASA api is down
             console.log("Could not load data today")
-        });
-}
+        })
+};
 
-// display the date, title, explanation and photo in a web page
-const showNasaPhoto = (photo) => {
-    document.getElementById('date').innerText = photo.date;
-    document.getElementById('title').innerText = photo.title;
-    document.getElementById('explanation').innerText = photo.explanation;
-    let imgFrame = document.createElement('img');
-    imgFrame.setAttribute('src', photo.url);
-    imgFrame.setAttribute('alt', "NASA photo of the day");
-    document.body.appendChild(imgFrame).classList.add('img');
-}
+// display the text in the web page
+const showText = (apiData) => {
+    document.getElementById('date').innerText = apiData.date
+    document.getElementById('title').innerText = apiData.title
+    document.getElementById('explanation').innerText = apiData.explanation
+};
 
-// display the date, title, explanation and video in a web page
-const showNasaVideo = (video) => {
-    document.getElementById('date').innerText = video.date;
-    document.getElementById('title').innerText = video.title;
-    document.getElementById('explanation').innerText = video.explanation;
-    let vidFrame = document.createElement('iframe');
-    vidFrame.setAttribute('src', video.url);
-    vidFrame.setAttribute('alt', "NASA video of the day");
-    vidFrame.style.width = "1024px";
-    vidFrame.style.height = "768px";
-    document.body.appendChild(vidFrame).classList.add('video');
-}
+
+// display the photo in the web page
+const showPhoto = (apiData) => {
+    let imgFrame = document.createElement('img')
+    imgFrame.setAttribute('src', apiData.url)
+    imgFrame.setAttribute('alt', "NASA photo of the day")
+    document.body.appendChild(imgFrame).classList.add('img')
+};
+
+// display the video in the web page
+const showVideo = (apiData) => {
+    let vidFrame = document.createElement('iframe')
+    vidFrame.setAttribute('src', apiData.url)
+    vidFrame.setAttribute('alt', "NASA video of the day")
+    vidFrame.style.width = "1024px"
+    vidFrame.style.height = "768px"
+    document.body.appendChild(vidFrame).classList.add('video')
+};
